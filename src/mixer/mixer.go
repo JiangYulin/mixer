a801f480842f492f3efd96c119c45ed4481b52a2
JiangYulin <jiangyulin118@gmail.com>
2019-03-19 17:49:55 +0800

    fit to background audio

--- src/mixer/mixer.go
+++ src/mixer/mixer.go
@@ -8,9 +8,11 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 
 	"../generator"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
@@ -18,7 +20,7 @@ import (
 
 type MixerAPI struct{}
 
-const CACHEFOLDER = "../tmp/"
+const CACHEFOLDER = "/tmp/mixer/"
 
 //根据实际机器配置
 const FFMPEG = "ffmpeg"
@@ -34,17 +36,34 @@ const OSS_SEC = "bxsoU8C4Ju4HTSqqyICnZxkVkRiNTC"
 const STATUS_SUCCESS = "success"
 const STATUS_FAIL = "fail"
 
-type AudioList struct {
-	Audios   []string `json:"audios"`
-	Str      string   `json:"str"`
-	FilePath [2]string
-	output   string
+type AudioObject struct {
+	Record struct {
+		Url      string `json:"url"`
+		Path     string
+		Duration float64
+	} `json:"record"`
+
+	Background struct {
+		Url           string `json:"url"`
+		Path          string
+		Duration      float64
+		FixedFilePath string //拼接或裁剪完的音频地址
+	} `json:"background"`
+
+	ConcatFileList string //ffmpeg 连接音频时需要使用的文件
+	output         string
 }
 
 type ResponseMessage struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 	Path    string `json:"path"`
+}
+
+type MediaInfo struct {
+	Format struct {
+		Duration string `json:"duration"`
+	} `json:"format"`
 }
 
 func (u *MixerAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -59,12 +78,36 @@ func (u *MixerAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
 func doPost(w http.ResponseWriter, r *http.Request) {
+
+	isExist, os_err := PathExists(CACHEFOLDER)
+
+	if os_err != nil {
+		HandleError(os_err, w)
+		return
+	}
+	if !isExist {
+		os_err := os.Mkdir(CACHEFOLDER, 0700)
+		if os_err != nil {
+			HandleError(os_err, w)
+			return
+		}
+	}
+
 	b, err := ioutil.ReadAll(r.Body)
 
 	defer r.Body.Close()
-	var t AudioList
+	var t AudioObject
 	if err != nil {
 		HandleError(err, w)
 	}
@@ -75,19 +118,89 @@ func doPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//判断数组长度为2,只能合成一遍数据
-	if len(t.Audios) != 2 {
+	if t.Record.Url == "" {
 		HandleError(errors.New("音频数量限定为2"), w)
 		return
 	}
 
-	//下载两个音频文件
-	for i := 0; i < len(t.Audios); i++ {
-		new_filename := generator.RandStringRunes(16)
-		t.FilePath[i] = CACHEFOLDER + new_filename
-		//保存文件到本地
-		err = DownloadFile(t.FilePath[i], t.Audios[i])
+	//下载录音
+	new_filename := generator.RandStringRunes(16)
+	t.Record.Path = CACHEFOLDER + new_filename
+	//保存文件到本地
+	err = DownloadFile(t.Record.Path, t.Record.Url)
+	if err != nil {
+		HandleError(err, w)
+		clearFiles(t)
+		return
+	}
+	//下载背景音乐
+	new_filename = generator.RandStringRunes(16)
+	t.Background.Path = CACHEFOLDER + new_filename + AUDIO_EXT
+	//保存文件到本地
+	err = DownloadFile(t.Background.Path, t.Background.Url)
+	if err != nil {
+		HandleError(err, w)
+		clearFiles(t)
+		return
+	}
+
+	// 判断音频长度，以决定是延长还是截断背景音乐
+	// ffprobe -i short.m4a -v quiet -print_format json -show_format -show_streams -hide_banner
+	// audio[0] 为录音、audio[1] 为背景
+	// ffprobe -i short.m4a -v quiet -print_format json -show_format -show_streams -hide_banner
+
+	t.Record.Duration = getAudioDuration(t.Record.Path)
+	t.Background.Duration = getAudioDuration(t.Background.Path)
+
+	if t.Record.Duration == -1 || t.Background.Duration == -1 {
+		HandleError(errors.New("解析音频长度出错"), w)
+		clearFiles(t)
+		return
+	}
+
+	/**
+	 * 背景音乐长度大于录音,直接截取背景音乐与录音相同
+	 **/
+
+	new_filename = generator.RandStringRunes(16)
+	t.Background.FixedFilePath = CACHEFOLDER + new_filename + AUDIO_EXT
+	if t.Record.Duration <= t.Background.Duration { //裁剪背景音乐
+
+		ffmpeg_cut_command := FFMPEG + " -i " + t.Background.Path + " -t " + fmt.Sprintf("%.2f", t.Record.Duration) + " " + t.Background.FixedFilePath
+		cmd := exec.Command(BASH, "-c", ffmpeg_cut_command)
+		err = cmd.Run()
 		if err != nil {
-			HandleError(errors.New("下载音频时出现错误"), w)
+			HandleError(err, w)
+			clearFiles(t)
+			return
+		}
+	} else {
+
+		x := int(math.Ceil(t.Record.Duration / t.Background.Duration))
+		new_filename = generator.RandStringRunes(16)
+		t.ConcatFileList = CACHEFOLDER + new_filename
+		f, err := os.Create(t.ConcatFileList)
+
+		if err != nil {
+			HandleError(err, w)
+			clearFiles(t)
+			return
+		}
+
+		for i := 0; i < x; i++ {
+			f.WriteString("file '" + t.Background.Path + "'\n")
+		}
+
+		defer f.Close()
+		f.Close()
+
+		//ffmpeg拼接音频
+		ffmpeg_concat_command := FFMPEG + " -f concat -safe 0 -i " + t.ConcatFileList + " -c copy " + t.Background.FixedFilePath
+		println(ffmpeg_concat_command)
+		cmd := exec.Command(BASH, "-c", ffmpeg_concat_command)
+		err = cmd.Run()
+		if err != nil {
+			HandleError(err, w)
 			clearFiles(t)
 			return
 		}
@@ -95,15 +208,12 @@ func doPost(w http.ResponseWriter, r *http.Request) {
 
 	output_filename := generator.RandStringRunes(16) + AUDIO_EXT
 	t.output = CACHEFOLDER + output_filename
-
-	ffmpeg_command := FFMPEG + " -i " + t.FilePath[0] + " -i " + t.FilePath[1] + " -filter_complex amerge -y " + t.output
+	ffmpeg_command := FFMPEG + " -i " + t.Record.Path + " -i " + t.Background.FixedFilePath + " -filter_complex amerge -y " + t.output
 	log.Println("run merge command, this will take several seconds.")
 	// log.Println(ffmpeg_command)
-
 	//准备合成
 	cmd := exec.Command(BASH, "-c", ffmpeg_command)
 	err = cmd.Run()
-
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 		HandleError(err, w)
@@ -150,15 +260,15 @@ func HandleError(err error, w http.ResponseWriter) {
 	fmt.Fprintf(w, string(str))
 }
 
-func clearFiles(obj AudioList) {
+func clearFiles(obj AudioObject) {
 	println("remove tmp files.")
-	err := os.Remove(obj.FilePath[0])
+	err := os.Remove(obj.Record.Path)
 	if err != nil {
-		println("remove file failed:", obj.FilePath[0])
+		println("remove file failed:", obj.Record.Path)
 	}
-	err = os.Remove(obj.FilePath[1])
+	err = os.Remove(obj.Background.Path)
 	if err != nil {
-		println("remove file failed:", obj.FilePath[1])
+		println("remove file failed:", obj.Background.Path)
 	}
 	err = os.Remove(obj.output)
 	if err != nil {
@@ -197,6 +307,7 @@ func DownloadFile(filepath string, url string) error {
 	// Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
+		println(1)
 		return err
 	}
 	defer out.Close()
@@ -204,6 +315,32 @@ func DownloadFile(filepath string, url string) error {
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+func getAudioDuration(path string) float64 {
+	// ffprobe -i short.m4a -v quiet -print_format json -show_format -show_streams -hide_banner
+
+	ffmpeg_command := "/usr/bin/ffprobe -i " + path + " -v quiet -print_format json -show_format -show_streams -hide_banner"
+	cmd := exec.Command(BASH, "-c", ffmpeg_command)
+	out, err := cmd.Output()
+	if err != nil {
+		println(err.Error())
+		return -1
+	}
+
+	var t MediaInfo
+	err = json.Unmarshal(out, &t)
+	if err == nil {
+		i, _err := strconv.ParseFloat(t.Format.Duration, 64)
+		if _err != nil {
+			println(_err.Error())
+			return -1
+		}
+		println(i)
+		return i
+	}
+	return 0
+
 }
 
 func execCommand(commandName string, params []string) bool {
